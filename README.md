92688732b33ba92d940f4aab6b6d7cce9ae90b12
ZhangXiaoMing <zhangxiaoming6672@163.com>
2020-08-13 20:40:32 +0800

    common lib

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Library_common
-Common library
+简单的BaseActivity和BaseAppView
+
+BaseActivity可以重写以下几个方法达到不同的效果：
+
+    1.isStatusBarTransparent()实现沉浸式状态栏效果；
+
+    2.getActivityOrientation()实现控制Activity的方向
---
+++ lib/src/main/java/com/zhang/library/common/view/BaseAppView.java
@@ -0,0 +1,70 @@
+package com.zhang.library.common.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.zhang.library.utils.utils.LogUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * 基类View
+ *
+ * @author ZhangXiaoMing 2020-08-13 15:27 星期四
+ */
+public abstract class BaseAppView extends FrameLayout
+        implements View.OnClickListener {
+
+    protected final String TAG = getClass().getSimpleName();
+
+    public BaseAppView(@NonNull Context context) {
+        super(context);
+        init(context);
+    }
+
+    public BaseAppView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public BaseAppView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    protected void init(Context context) {
+        LayoutInflater.from(context).inflate(onCreateLayoutId(), this);
+
+        initView();
+        initData();
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        LogUtils.info(TAG, "onAttachedToWindow()");
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        LogUtils.info(TAG, "onDetachedFromWindow()");
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+
+    /** 获取View的布局 */
+    protected abstract int onCreateLayoutId();
+
+    /** 初始化控件 */
+    protected abstract void initView();
+
+    /** 初始化数据 */
+    protected abstract void initData();
+}
