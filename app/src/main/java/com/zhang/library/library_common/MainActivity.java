c0f1290d615b455752bc1c9a16103144e7b078ed
ZhangXiaoMing <zhangxiaoming6672@163.com>
2020-11-02 11:41:47 +0800

    调试代码

--- app/src/main/java/com/zhang/library/library_common/MainActivity.java
+++ app/src/main/java/com/zhang/library/library_common/MainActivity.java
@@ -1,8 +1,19 @@
 package com.zhang.library.library_common;
 
+import android.os.Bundle;
+
 import com.zhang.library.common.activity.BaseActivity;
+import com.zhang.library.utils.LogUtils;
+
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        LogUtils.setDebug(true);
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     protected int onCreateLayoutId() {
@@ -16,6 +27,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 
+        TestFragment fragment = new TestFragment();
+
+        ft.add(R.id.fl_container, fragment, fragment.getFragmentName());
+
+        ft.commitAllowingStateLoss();
     }
 }
---
+++ app/src/main/java/com/zhang/library/library_common/TestFragment.java
@@ -0,0 +1,23 @@
+package com.zhang.library.library_common;
+
+import com.zhang.library.common.fragment.BaseFragment;
+
+/**
+ * @author ZhangXiaoMing 2020-11-02 11:21 星期一
+ */
+public class TestFragment extends BaseFragment {
+    @Override
+    protected int onCreateLayoutId() {
+        return R.layout.fragment_test;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
