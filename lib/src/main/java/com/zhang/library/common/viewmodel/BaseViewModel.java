a6813621ad766044d871e6e895b1d97f4d173ae1
xiaoming6672 <zhangxiaoming6672@163.com>
2023-08-23 21:54:23 +0800

    BaseViewModel调整,添加Disposable列表，在onClear的时候移除

--- lib/src/main/java/com/zhang/library/common/viewmodel/BaseViewModel.java
+++ lib/src/main/java/com/zhang/library/common/viewmodel/BaseViewModel.java
@@ -9,7 +9,14 @@ import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModel;
 
+import com.zhang.library.utils.CollectionUtils;
+
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import io.reactivex.rxjava3.disposables.Disposable;
 
 /**
  * 封装的ViewModel
@@ -25,12 +32,15 @@ public class BaseViewModel extends ViewModel
     protected WeakReference<Fragment> mFragment;
     protected WeakReference<DialogFragment> mDialog;
 
+    private final List<Disposable> mDisposableList;
+
     {
         TAG = getClass().getName();
     }
 
     public BaseViewModel(@NonNull FragmentActivity activity) {
         this.mActivity = new WeakReference<>(activity);
+        this.mDisposableList = new ArrayList<>();
 
         activity.getLifecycle().addObserver(this);
     }
@@ -38,6 +48,7 @@ public class BaseViewModel extends ViewModel
     public BaseViewModel(@NonNull Fragment fragment) {
         this.mActivity = new WeakReference<>(fragment.requireActivity());
         this.mFragment = new WeakReference<>(fragment);
+        this.mDisposableList = new ArrayList<>();
 
         fragment.getLifecycle().addObserver(this);
     }
@@ -45,14 +56,35 @@ public class BaseViewModel extends ViewModel
     public BaseViewModel(@NonNull DialogFragment dialog) {
         this.mActivity = new WeakReference<>(dialog.requireActivity());
         this.mDialog = new WeakReference<>(dialog);
+        this.mDisposableList = new ArrayList<>();
 
         dialog.getLifecycle().addObserver(this);
+    }
+
+    protected void addDisposable(Disposable disposable) {
+        mDisposableList.add(disposable);
+    }
+
+    protected void clearDisposable() {
+        if (CollectionUtils.isEmpty(mDisposableList))
+            return;
+
+        Iterator<Disposable> it = mDisposableList.iterator();
+        while (it.hasNext()) {
+            Disposable next = it.next();
+            if (!next.isDisposed())
+                next.dispose();
+
+            it.remove();
+        }
     }
 
 
     @Override
     protected void onCleared() {
         super.onCleared();
+
+        clearDisposable();
 
         if (mDialog != null) {
             mDialog.clear();
